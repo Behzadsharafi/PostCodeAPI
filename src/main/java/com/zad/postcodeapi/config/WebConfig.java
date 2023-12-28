52ac5b9d84b213f623350ad2e87bfaa123094127
Behzad Sharafi <behzadsharafi@gmail.com>
2023-12-28 17:41:16 +1100

    adds 5176 to allowed origins

--- src/main/java/com/zad/postcodeapi/config/WebConfig.java
+++ src/main/java/com/zad/postcodeapi/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
 	public void addCorsMappings(CorsRegistry registry) {
 
-		String[] allowedOrigins = { "http://localhost:5173/","http://localhost:5174/", "http://127.0.0.1:5173/","http://127.0.0.1:5174/" };
+		String[] allowedOrigins = { "http://localhost:5173/", "http://localhost:5174/", "http://localhost:5175/",
+				"http://localhost:5176/", "http://127.0.0.1:5173/", "http://127.0.0.1:5174/" };
 
 		registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("GET", "POST", "DELETE", "PATCH")
 				.allowedHeaders("*");
